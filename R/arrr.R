adc05ff52691530b0bad3b15aa7e72efd6f80a99
Jim Hester <james.f.hester@gmail.com>
2014-09-19 16:17:56 -0400

    We must remove all sexism!
    sub only substitutes the first instance, need to use `gsub` to substitute if there are more than one!

--- R/arrr.R
+++ R/arrr.R
@@ -5,5 +5,5 @@
 arrr = function(text) {
   URL = paste0('http://isithackday.com/arrpi.php?text=', URLencode(text))
   out = content(GET(URL), as="text")
-  return(sub("wench", "lass", out))
+  return(gsub("wench", "lass", out))
 }
